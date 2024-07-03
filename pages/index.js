6fa566098ba65223114c5de6ba13123b98291fd8
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-07-03 19:19:51 +0530

    add home cards

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,22 @@ import toast from "react-hot-toast";
 //INTERNAL IMPORT
 import { useStateContext } from "../Context/index";
 import Header from "../Components/Header";
-
+import Input from "../Components/Input";
+import Button from "../Components/Button";
+import Table from "../Components/Table";
+import PreSaleList from "../Components/PreSaleList";
+import UploadLogo from "../Components/UploadLogo";
 import Loader from "../Components/Loader";
 import Footer from "../Components/Footer";
+import ICOMarket from "../Components/ICOMarket";
+import TokenCreator from "../Components/TokenCreator";
+import TokenHistory from "../Components/TokenHistory";
+import Marketplace from "../Components/Marketplace";
+import CreateICO from "../Components/CreateICO";
+import Card from "../Components/Card";
+import BuyToken from "../Components/BuyToken";
+import WidthdrawToken from "../Components/WidthdrawToken";
+import TokenTransfer from "../Components/TokenTransfer";
 
 const index = () => {
   const {
@@ -93,7 +106,97 @@ const index = () => {
         setOpenICOMarketplace={setOpenICOMarketplace}
         openICOMarketplace={openICOMarketplace}
       />
-      
+      <div className="create">
+        <h1 style={{ fontSize: "2rem" }}>All ICOs Marketplace</h1>
+        {allICOs?.length != 0 && (
+          <Marketplace
+            array={allICOs}
+            shortenAddress={shortenAddress}
+            setBuyIco={setBuyIco}
+            setOpenBuyToken={setOpenBuyToken}
+            currency={currency}
+          />
+        )}
+        <Card
+          setOpenAllICO={setOpenAllICO}
+          setOpenTokenCreator={setOpenTokenCreator}
+          setOpenTransferToken={setOpenTransferToken}
+          setOpenTokenHistory={setOpenTokenHistory}
+          setopenWidthdrawToken={setopenWidthdrawToken}
+          setOpenICOMarketplace={setOpenICOMarketplace}
+          copyAddress={copyAddress}
+          setOpenCreateICO={setOpenCreateICO}
+        />
+      </div>
+      {openAllICO && (
+        <ICOMarket
+          array={allUserIcos}
+          shortenAddress={shortenAddress}
+          handleClick={setOpenAllICO}
+          currency={currency}
+        />
+      )}
+      {openTokenCreator && (
+        <TokenCreator
+          createERC20={createERC20}
+          shortenAddress={shortenAddress}
+          setOpenTokenCreator={setOpenTokenCreator}
+          setLoader={setLoader}
+          address={address}
+          connectWallet={connectWallet}
+          PINATA_AIP_KEY={PINATA_AIP_KEY}
+          PINATA_SECRECT_KEY={PINATA_SECRECT_KEY}
+        />
+      )}
+      {openTokenHistory && (
+        <TokenHistory
+          shortenAddress={shortenAddress}
+          setOpenTokenHistory={setOpenTokenHistory}
+        />
+      )}
+      {openCreateICO && (
+        <CreateICO
+          shortenAddress={shortenAddress}
+          setOpenCreateICO={setOpenCreateICO}
+          connectWallet={connectWallet}
+          address={address}
+          createICOSale={createICOSale}
+        />
+      )}
+      {openICOMarketplace && (
+        <ICOMarket
+          array={allICOs}
+          shortenAddress={shortenAddress}
+          handleClick={setOpenICOMarketplace}
+          currency={currency}
+        />
+      )}
+      {openBuyToken && (
+        <BuyToken
+          address={address}
+          buyToken={buyToken}
+          connectWallet={connectWallet}
+          setOpenBuyToken={setOpenBuyToken}
+          buyIco={buyIco}
+          currency={currency}
+        />
+      )}
+      {openTransferToken && (
+        <TokenTransfer
+          address={address}
+          transferTokens={transferTokens}
+          connectWallet={connectWallet}
+          setOpenTransferToken={setOpenTransferToken}
+        />
+      )}
+      {openWidthdrawToken && (
+        <WidthdrawToken
+          address={address}
+          withdrawToken={withdrawToken}
+          connectWallet={connectWallet}
+          setopenWidthdrawToken={setopenWidthdrawToken}
+        />
+      )}
 
       <Footer />
       {loader && <Loader />}
